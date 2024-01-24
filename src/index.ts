07a6a3b9b9f029f205e3770915cc395e5fc8e5d7
T1CkShot <fatta.mmg@gmail.com>
2024-01-25 02:54:17 +0600

    set a command handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import * as path from "path";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -8,3 +11,39 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.login(process.env.PRIVATE_BOT_TOKEN);
+
+// COMMAND HANDLER
+
+// Define __dirname
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(fileName);
+
+// folderPath and commandFolders array
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+// client commands property
+client.commands = new Collection();
+
+// return the files for each folder in commandFolders in an array
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const files = fs.readdirSync(commandsPath).filter((file) => {
+    file.endsWith(".js");
+  });
+
+  // require each file
+  for (const file in files) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    // set them in client.commands property
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[Warning] the command at ${filePath} is missing 'data' or 'execute' property`,
+      );
+    }
+  }
+}
