887e6cbe1c32c3edbebe3398170843122cd3296e
T1CkShot <fatta.mmg@gmail.com>
2024-01-26 22:40:21 +0600

    fix Command Type

--- src/@types/app.d.ts
+++ src/@types/app.d.ts
@@ -3,6 +3,6 @@ import { Command } from "../utils/types";
 
 declare module "discord.js" {
   export interface Client {
-    commands: Collection<string, Command>;
+    commands: Collection<unknown, Command>;
   }
 }
