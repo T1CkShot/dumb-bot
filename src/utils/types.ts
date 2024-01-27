f427696aa9aa166c6416bea5666f86da413639a5
T1CkShot <fatta.mmg@gmail.com>
2024-01-27 23:53:07 +0600

    fix execute return type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,13 @@
 import {
   CommandInteraction,
+  InteractionResponse,
   SlashCommandBuilder,
   SlashCommandOptionsOnlyBuilder,
 } from "discord.js";
 
 export default interface Command {
   data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
-  execute: (interaction: CommandInteraction) => Promise<void>;
+  execute: (
+    interaction: CommandInteraction,
+  ) => Promise<InteractionResponse<boolean>>;
 }
